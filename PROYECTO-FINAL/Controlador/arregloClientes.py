b43674c49d3cede1d89775eadd2961d3a296f705
Alvaro Vidal <97235272+AlvaroVidal21@users.noreply.github.com>
2025-08-14 01:56:00 -0500

    Persist domain data in CSV files

--- PROYECTO-FINAL/Controlador/arregloClientes.py
+++ PROYECTO-FINAL/Controlador/arregloClientes.py
@@ -1,37 +1,72 @@
 from Controlador.clientes import cliente
+import csv
+import os
+
 #Mantenimiento para los clientes
 
-class ArregloClientes():
+class ArregloClientes:
 
-    #Atributo
-    dataCliente = [] #<-- Nuestra base de datos
-
-    #Constructor (vacio)
     def __init__(self):
-        pass
+        # ruta del archivo csv donde se almacenarán los datos
+        self.__archivo = os.path.join(os.path.dirname(__file__), 'clientes.csv')
+        self.dataCliente = []  # <-- Nuestra base de datos en memoria
+        self.cargar()  # Cargar datos existentes del archivo
 
-    def adicionaCliente(self,objcli): # Graba los datos de los clientes
+    # ---------------------------
+    # Métodos de persistencia
+    # ---------------------------
+    def cargar(self):
+        """Carga los datos desde el archivo CSV."""
+        if os.path.exists(self.__archivo):
+            with open(self.__archivo, newline='', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if row:  # evitar filas vacías
+                        obj = cliente(*row)
+                        self.dataCliente.append(obj)
+
+    def grabar(self):
+        """Guarda los datos actuales en el archivo CSV."""
+        with open(self.__archivo, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            for cli in self.dataCliente:
+                writer.writerow([
+                    cli.getDniCliente(),
+                    cli.getNombresCliente(),
+                    cli.getApellidoPaternoCliente(),
+                    cli.getApellidoMaternoCliente(),
+                    cli.getDireccionCliente(),
+                    cli.getTelefonoCliente(),
+                ])
+
+    # ---------------------------
+    # Métodos de gestión
+    # ---------------------------
+    def adicionaCliente(self, objcli):  # Graba los datos de los clientes
         self.dataCliente.append(objcli)
+        self.grabar()
 
-    def devolverCliente(self, pos): # Retorna los datos de los clientes
+    def devolverCliente(self, pos):  # Retorna los datos de los clientes
         return self.dataCliente[pos]
 
-    def tamañoArregloCliente(self): # Devuelve los datos de los clientes
+    def tamañoArregloCliente(self):  # Devuelve los datos de los clientes
         return len(self.dataCliente)
 
-    def buscarCliente(self, dni): # Busca los clientes por dni
-        for i in range (self.tamañoArregloCliente()):
+    def buscarCliente(self, dni):  # Busca los clientes por dni
+        for i in range(self.tamañoArregloCliente()):
             if dni == self.dataCliente[i].getDniCliente():
-                return i # devuelve la posicion del elemento encontrado
+                return i  # devuelve la posicion del elemento encontrado
         return -1  # signfica que no encontro ningun dato
 
-    def eliminarCliente(self, pos): # Elimina los clientes
-        del(self.dataCliente[pos])
+    def eliminarCliente(self, pos):  # Elimina los clientes
+        del (self.dataCliente[pos])
+        self.grabar()
 
-    def modificarCliente(self, objcli, pos): # Modifica datos de los clientes
+    def modificarCliente(self, objcli, pos):  # Modifica datos de los clientes
         self.dataCliente[pos] = objcli
+        self.grabar()
 
-    def retornarDatos(self): # retorna los datos de los clientes
+    def retornarDatos(self):  # retorna los datos de los clientes
         return self.dataCliente
 
     
--- PROYECTO-FINAL/Controlador/arregloEmpleados.py
+++ PROYECTO-FINAL/Controlador/arregloEmpleados.py
@@ -1,37 +1,68 @@
 from Controlador.empleados import empleado
+import csv
+import os
+
 #Mantenimiento para los empleados
 
-class ArregloEmpleados():
+class ArregloEmpleados:
 
-    #Atributo
-    dataEmpleado = [] #<-- Nuestra base de datos
-
-    #Constructor (vacio)
     def __init__(self):
-        pass
+        self.__archivo = os.path.join(os.path.dirname(__file__), 'empleados.csv')
+        self.dataEmpleado = []  # <-- Nuestra base de datos en memoria
+        self.cargar()
 
-    def adicionaEmpleado(self,objcli): # Graba los datos de los Empleados
+    # ---------------------------
+    # Métodos de persistencia
+    # ---------------------------
+    def cargar(self):
+        if os.path.exists(self.__archivo):
+            with open(self.__archivo, newline='', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if row:
+                        self.dataEmpleado.append(empleado(*row))
+
+    def grabar(self):
+        with open(self.__archivo, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            for emp in self.dataEmpleado:
+                writer.writerow([
+                    emp.getDniEmpleado(),
+                    emp.getNombresEmpleado(),
+                    emp.getApellidoPaternoEmpleado(),
+                    emp.getApellidoMaternoEmpleado(),
+                    emp.getDireccionEmpleado(),
+                    emp.getTelefonoEmpleado(),
+                ])
+
+    # ---------------------------
+    # Métodos de gestión
+    # ---------------------------
+    def adicionaEmpleado(self, objcli):  # Graba los datos de los Empleados
         self.dataEmpleado.append(objcli)
+        self.grabar()
 
-    def devolverEmpleado(self, pos): # Retorna los datos de los Empleados
+    def devolverEmpleado(self, pos):  # Retorna los datos de los Empleados
         return self.dataEmpleado[pos]
 
-    def tamañoArregloEmpleado(self): # Devuelve los datos de los Empleados
+    def tamañoArregloEmpleado(self):  # Devuelve los datos de los Empleados
         return len(self.dataEmpleado)
 
-    def buscarEmpleado(self, dni): # Busca los Empleados por dni
-        for i in range (self.tamañoArregloEmpleado()):
+    def buscarEmpleado(self, dni):  # Busca los Empleados por dni
+        for i in range(self.tamañoArregloEmpleado()):
             if dni == self.dataEmpleado[i].getDniEmpleado():
-                return i # devuelve la posicion del elemento encontrado
+                return i  # devuelve la posicion del elemento encontrado
         return -1  # signfica que no encontro ningun dato
 
-    def eliminarEmpleado(self, pos): # Elimina los Empleados
-        del(self.dataEmpleado[pos])
+    def eliminarEmpleado(self, pos):  # Elimina los Empleados
+        del (self.dataEmpleado[pos])
+        self.grabar()
 
-    def modificarEmpleado(self, objcli, pos): # Modifica datos de los Empleados
+    def modificarEmpleado(self, objcli, pos):  # Modifica datos de los Empleados
         self.dataEmpleado[pos] = objcli
+        self.grabar()
 
-    def retornarDatos(self): # retorna los datos de los Empleados
+    def retornarDatos(self):  # retorna los datos de los Empleados
         return self.dataEmpleado
 
     
